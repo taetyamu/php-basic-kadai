a884c10e5bfb0e403628dddcb38ff757d3180223
akane <167318350+taetyamu@users.noreply.github.com>
2024-10-10 21:08:11 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>課題７</title>
+ </head>
+ 
+ <body>
+     <p>
+     <?php
+$product = array(
+    "name" => "onion",
+    "price" => 200,
+    "weight" => 160
+);
+
+print_r($product);
+?>
+
+     </p>
+ </body>
+ 
+ </html>
